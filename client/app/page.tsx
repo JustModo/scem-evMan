f6e6d976a32baa0892b527d7d89877500ab257c5
Yash <yashlaxman@gmail.com>
2025-04-28 21:49:48 +0530

    add: basic ui components

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,7 +1,19 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import React from "react";
 
 // Landing Page
 // Landing page for the platform. Highlight purpose, CTA for login or join contest.
 export default function LandingPage() {
-  return <div>LandingPage Screen</div>;
+  return (
+    <div>
+      LandingPage Screen
+      <Button>
+        <Link href={"/admin"}>Admin</Link>
+      </Button>
+      <Button>
+        <Link href={"/test/1"}>Tests</Link>
+      </Button>
+    </div>
+  );
 }
