b902336ce3207bfc7a2395f09feb763eea04bd08
Modo <yashlaxman@gmail.com>
2026-01-06 23:01:00 +0530

    feat: containerize the server with dev and prod mode

--- server/index.js
+++ server/index.js
@@ -1,18 +1,20 @@
-const express =require('express')
+const express = require('express')
 require('dotenv').config()
-const cors=require('cors')
-const app=express();
-const compRoutes=require('./routes/compilerRoutes');
-const testRoutes=require('./routes/contestRoutes');
-const adminRoutes=require('./routes/adminRoutes');
+const cors = require('cors')
+const app = express();
+const compRoutes = require('./routes/compilerRoutes');
+const testRoutes = require('./routes/contestRoutes');
+const adminRoutes = require('./routes/adminRoutes');
+const { connectDB } = require('./controllers/dbCon');
+connectDB();
 require('./controllers/userCon')
-const port=process.env.PORT|8080;
+const port = process.env.PORT || 8080;
 app.use(cors())
 app.use(express.json())
-app.get('/',(req,res)=>{res.send("SOSCEvMan API is running...")})
-app.use('/cmp',compRoutes);
-app.use('/contest',testRoutes);
-app.use('/api/admin',adminRoutes);
-app.listen(port,()=>{
+app.get('/', (req, res) => { res.send("SOSCEvMan API is running...") })
+app.use('/cmp', compRoutes);
+app.use('/contest', testRoutes);
+app.use('/api/admin', adminRoutes);
+app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 })
