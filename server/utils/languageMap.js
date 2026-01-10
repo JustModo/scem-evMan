e4198c2838c9a393fce48ab0765616569a22ff03
rajaykumar <rajaykumar12959@gmail.com>
2026-01-10 23:20:23 +0530

    feat: language map

--- server/utils/languageMap.js
+++ server/utils/languageMap.js
@@ -1,13 +1,8 @@
 // Map of language names to Judge0 language IDs (e.g., Python -> 71)
 const languageMap = {
     "c": 50,
-    "cpp": 54,
     "java": 62,
     "python": 71,
-    "javascript": 63,
-    "c++": 54,
-    "py": 71,
-    "js": 63,
 };
 
 module.exports = { languageMap };
